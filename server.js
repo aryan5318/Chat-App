c66402ee1a815b063f09c093f4799e0f07dc0cb8
aryan5318 <codev7127@gmail.com>
2025-12-15 17:13:13 +0530

    day2

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app= express();
 const Port=3000;
 const server=createServer(app);
 
+app.use(express.static("public"));
+
 // intialize new instance of socket.io by passing server object
 const io=new Server(server);
-app.get('/',(req,res)=>{
-    res.sendFile(join(__dirname,'index.html'))
-})
+
 
 // Listen on the connection event for incoming sockets and log it to the console 
 
@@ -23,11 +23,18 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log("user disconnected");
     })
-     socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
+    socket.emit("welcome",{ 
+      socketId:socket.id
+    }
+    )
+     socket.on('chat message', (data) => {
+    console.log('message: ' + data);
 
 // sending message to every one including the sender;
-    io.emit('chat message',msg);
+    io.emit("chat message", {
+      message: data.message,
+      socketId: socket.id // trusted from server
+    });
     
   });
 })
